31c7b633981245faf4af600c6bb28135b5a7ea19
lie2983 <lie2983@CA-SU-F106-24.sduhsd.lan>
2018-02-02 13:57:55 -0800

    Upload APCSA School Project

---
+++ APCSA/src/Unit0/Lab0a.java
@@ -0,0 +1,20 @@
+package Unit0;
+
+public class Lab0a {
+	public static void main ( String[] args )
+	{
+		System.out.println("Edward Li \t  02/02/18 \t Period 4\n\n" );
+		System.out.println("Owl" );
+		System.out.println("\n\n\n\n" );
+
+
+		System.out.println(" /\\_/\\\n((@v@))\n():::()\n VV-VV");
+		//add other output
+
+		System.out.println(" \n\n\n\nHelpFul Hints" );
+		System.out.println("\\\\ draws one backslash on the screen!\n" );
+		System.out.println("\\\" draws one double quote on the screen!\n" );
+		System.out.println("\\\' draws one single quote on the screen!\n" );
+	}
+
+}
---
+++ APCSA/src/Unit0/Lab0b.java
@@ -0,0 +1,55 @@
+package Unit0;
+
+public class Lab0b
+{
+	public static void main ( String[] args )
+	{
+		//define 1 variable of each of the
+		//following data types
+		//byte		short		int 		long
+		//float		double
+		//char      boolean		String
+
+		//integer variables
+		byte byteOne = 127;
+		short shortOne = 12345;
+		int intOne = 12345678;
+		long longOne = 1234245345;
+
+
+		//decimal variables
+		float floatOne = 100000.0f;
+		double doubleOne = 1000000.00;
+
+		//other integer types
+		char charOne = 'a';
+		char charTwo = 97;
+		//lower case a: 97, A: 65, 0: 49
+
+		//other types
+		boolean boolOne = true;
+		
+		String stringOne = "asdfasdf";
+		
+
+		//output your information here
+		System.out.println("/////////////////////////////////");
+		System.out.println("*Jim Bob                08/18/06*");
+		System.out.println("*                               *");
+		System.out.println("*        integer types          *");
+		System.out.println("*                               *");
+		System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+}
