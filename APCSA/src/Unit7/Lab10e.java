442b88f4dd9fa69c26433370285eb69ff54a4eb5
lie2983 <lie2983@CA-SU-F106-24.sduhsd.lan>
2018-02-20 15:00:09 -0800

    lab10e update

---
+++ APCSA/src/Unit7/Lab10e.java
@@ -0,0 +1,21 @@
+package Unit7;
+
+import java.util.Scanner;
+import static java.lang.System.*;
+
+public class Lab10e
+{
+	public static void main(String args[])
+	{
+		System.out.println("Choose an upper bound");
+		Scanner keyboard = new Scanner(System.in);
+		int response;
+		response = keyboard.nextInt();
+
+		GuessingGame test = new GuessingGame(response);
+		test.playGame();
+
+
+
+	}
+}
