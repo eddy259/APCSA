456415dc104e49153e4c8ba2ff3ba8d828be8a97
ED2016 <ED2016@LAPTOP-11L6942D.sduhsd.lan>
2018-03-06 13:45:25 -0800

    unit 1-10 working

---
+++ APCSA/src/Unit10/Lab16d.java
@@ -0,0 +1,13 @@
+package Unit10;
+
+import static java.lang.System.*;
+
+public class Lab16d
+{
+	public static void main( String args[] )
+	{
+		//make a new MadLib
+		
+		out.println("\n");
+	}
+}
