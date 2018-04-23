7e1a126652a26ff657d550dd973bb6d44cf49cf9
lie2983 <lie2983@CA-SU-F106-24.sduhsd.lan>
2018-04-23 15:00:52 -0700

    starfighter working

---
+++ APCSA/src/Unit17/Ship.java
@@ -0,0 +1,64 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+package Unit17;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.io.File;
+import javax.imageio.ImageIO;
+
+public class Ship extends MovingThing
+{
+	private int speed;
+	private Image image;
+
+	public Ship()
+	{
+		this(0,0,0);
+	}
+
+	public Ship(int x, int y)
+	{
+		this(x,y,0);
+	}
+
+	public Ship(int x, int y, int s)
+	{
+		super(x, y);
+		speed = s;
+		try
+		{
+			image = ImageIO.read(new File("\\C:\\Users\\lie2983\\Desktop\\APCSAGitRepository\\APCSA\\src\\Unit17\\ship.jpg"));
+		}
+		catch(Exception e)
+		{
+			//feel free to do something here
+			System.out.println(e);
+		}
+	}
+
+
+	public void setSpeed(int s)
+	{
+	   //add more code
+		speed = s;
+	}
+
+	public int getSpeed()
+	{
+	   return speed;
+	}
+
+	public void draw( Graphics window )
+	{
+		window.drawImage(image,getX(),getY(),80,80,null);
+	}
+
+	public String toString()
+	{
+		return super.toString() + getSpeed();
+	}
+}
